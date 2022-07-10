2aca3f3256c1b8e9b9f3807e937d18944dd537fb
Volodya Ismailyan <nenie_iri@mail.ru>
2022-07-10 17:01:38 +0400

    Complete first exercise

---
+++ CPP_Module_00/ex00/megaphone.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vismaily <nenie_iri@mail.ru>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/10 15:35:43 by vismaily          #+#    #+#             */
+/*   Updated: 2022/07/10 16:34:38 by vismaily         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(int argc, char **argv)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	while (++i < argc)
+	{
+		j = -1;
+		while (argv[i][++j] != '\0')
+			std::cout << (char)toupper(argv[i][j]);
+	}
+	std::cout << std::endl;
+	return (0);
+}
