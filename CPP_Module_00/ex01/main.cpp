aa5fc557e2e633f190d13b5446d376ad84a41322
Volodya Ismailyan <nenie_iri@mail.ru>
2022-07-11 20:36:30 +0400

    Add part of ADD function

--- CPP_Module_00/ex01/main.cpp
+++ CPP_Module_00/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: vismaily <nenie_iri@mail.ru>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/11 14:39:31 by vismaily          #+#    #+#             */
-/*   Updated: 2022/07/11 19:13:01 by vismaily         ###   ########.fr       */
+/*   Updated: 2022/07/11 20:33:10 by vismaily         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,45 @@
 #include <string>
 #include "Contact.hpp"
 
+void	is_valid(std::string str, index, &i)
+{
+	if (i != 0)
+	{
+		if (std::strlen() == 0)
+	}
+}
+
 void	add_contact()
 {
-	std::string	fields[5];	
+	int			i;
+	std::string	fields[5];
 
-	std::cout << "First name: ";
-	std::getline(std::cin, fields[0]);
-	std::cout << "Last name: ";
-	std::getline(std::cin, fields[1]);
-	std::cout << "Nickname: ";
-	std::getline(std::cin, fields[2]);
-	std::cout << "Phone number: ";
-	std::getline(std::cin, fields[3]);
-	std::cout << "Darkest secret: ";
-	std::getline(std::cin, fields[4]);
+	i = 0;
+	while (is_valid(fields[0], 0, i) == 0)
+	{
+		std::cout << "First name: ";
+		std::getline(std::cin, fields[0]);
+	}
+	while (is_valid(fields[1], 1, i) == 0)
+	{
+		std::cout << "Last name: ";
+		std::getline(std::cin, fields[1]);
+	}
+	while (is_valid(fields[2], 2, i) == 0)
+	{
+		std::cout << "Nickname: ";
+		std::getline(std::cin, fields[2]);
+	}
+	while (is_valid(fields[3], 3, i) == 0)
+	{
+		std::cout << "Phone number: ";
+		std::getline(std::cin, fields[3]);
+	}
+	while (is_valid(fields[4], 4, i) == 0)
+	{
+		std::cout << "Darkest secret: ";
+		std::getline(std::cin, fields[4]);
+	}
 }
 
 int	main()
