059733b9782aabf7761be93afcc9bbb352f0d899
Volodya Ismailyan <nenie_iri@mail.ru>
2022-07-15 16:29:55 +0400

    CPP_01_ex00 Write Zombie.hpp

--- CPP_Module_01/ex00/Zombie.hpp
+++ CPP_Module_01/ex00/Zombie.hpp
@@ -6,12 +6,21 @@
 /*   By: vismaily <nenie_iri@mail.ru>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/15 16:17:25 by vismaily          #+#    #+#             */
-/*   Updated: 2022/07/15 16:22:28 by vismaily         ###   ########.fr       */
+/*   Updated: 2022/07/15 16:28:35 by vismaily         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+# include <string>
+
 class	Zombie
 {
-	Private:
+	public:
+		void		announce(void);
+	private:
 		std::string	name;
 }
+
+#endif
