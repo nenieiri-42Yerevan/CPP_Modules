02d166025d40b24027b562a0a3691e55df26983a
Volodya Ismailyan <nenie_iri@mail.ru>
2022-08-11 16:27:20 +0400

    CPP_07_ex02

---
+++ CPP_Module_07/ex02/Array.hpp
@@ -0,0 +1,101 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vismaily <nenie_iri@mail.ru>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/11 13:20:09 by vismaily          #+#    #+#             */
+/*   Updated: 2022/08/11 16:18:27 by vismaily         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ARRAY_HPP
+# define ARRAY_HPP
+
+# include <stdexcept>
+# include <iostream>
+
+template	<typename T>
+class		Array
+{
+	public:
+		Array();
+		Array(unsigned int n);
+		Array(const Array &rhs);
+		Array			&operator=(const Array &rhs);
+		~Array();
+	public:
+		T				&operator[](unsigned int i) const;
+		unsigned int	size() const;
+	private:
+		T				*_arr;
+		unsigned int	_size;
+};
+
+template	<typename T>
+Array<T>::Array() : _arr(0), _size(0)
+{
+}
+
+template	<typename T>
+Array<T>::Array(unsigned int n) : _arr(new T[n]), _size(n)
+{
+}
+
+template	<typename T>
+Array<T>::Array(const Array &rhs) : _arr(0), _size(rhs._size)
+{
+	if (_size != 0)
+	{
+		_arr = new T[_size];
+		for	(unsigned int i = 0; i < rhs._size; ++i)
+			this->_arr[i] = rhs._arr[i];
+	}
+}
+
+template	<typename T>
+Array<T>::~Array()
+{
+	delete [] this->_arr;
+}
+
+template	<typename T>
+Array<T>	&Array<T>::operator=(const Array &rhs)
+{
+	if (this != &rhs)
+	{
+		if (this->_size > 0)
+			delete [] this->_arr;
+
+		this->_size = rhs._size;
+		this->_arr = new T[_size];
+		for (unsigned int i = 0; i < rhs._size; ++i)
+			this->_arr[i] = rhs._arr[i];
+	}
+
+	return (*this);
+}
+
+template	<typename T>
+T			&Array<T>::operator[](unsigned int idx) const
+{
+	try
+	{
+		if (idx < 0 || idx >= this->_size)
+			throw std::range_error("index out of bounds");
+	}
+	catch (const std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	return (this->_arr[idx]);
+}
+
+template		<typename T>
+unsigned int	Array<T>::size() const
+{
+	return (this->_size);
+}
+
+#endif
---
+++ CPP_Module_07/ex02/main.cpp
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vismaily <nenie_iri@mail.ru>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/11 13:20:06 by vismaily          #+#    #+#             */
+/*   Updated: 2022/08/11 16:26:13 by vismaily         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+#include "Array.hpp"
+
+int	main()
+{
+	Array<int>	*int_arr = new Array<int>();
+
+	std::cout << "int_arr size: " << int_arr->size() << std::endl << std::endl;
+	delete int_arr;
+
+	Array<int>	arr_1(5);
+	for (unsigned int i = 0; i < arr_1.size(); ++i)
+		arr_1[i] = i * 2;
+
+	std::cout << "arr_1 size: " << arr_1.size() << std::endl;
+	for (unsigned int i = 0; i < arr_1.size(); ++i)
+		std::cout << "arr_1 [" << i << "] = " << arr_1[i] << std::endl;
+
+	std::cout << std::endl;
+
+	Array<int>	*copy_arr_1 = new Array<int>(arr_1);
+	for (unsigned int i = 0; i < copy_arr_1->size(); ++i)
+		std::cout << "copy_arr_1 [" << i << "] = " << (*copy_arr_1)[i] << std::endl;
+
+	std::cout << std::endl;
+
+	Array<int>	arr_2(9);
+	for (unsigned int i = 0; i < arr_2.size(); ++i)
+		arr_2[i] = i;
+
+	arr_1 = arr_2;
+
+	std::cout << "arr_1 size: " << arr_1.size() << std::endl;
+	for (unsigned int i = 0; i < arr_1.size(); ++i)
+		std::cout << "arr_1 [" << i << "] = " << arr_1[i] << std::endl;
+
+	std::cout << std::endl << arr_1[arr_1.size()] << std::endl << std::endl;
+
+	Array<std::string>	str_arr(3);
+	str_arr[0] = "Nenie";
+	str_arr[1] = " ";
+	str_arr[2] = "Iri";
+
+	std::string	line = str_arr[0] + str_arr[1] + str_arr[2];
+
+	std::cout << "Str size: " << str_arr.size() << std::endl;
+	std::cout << "Str: " << line << std::endl;
+
+	delete copy_arr_1;
+
+	return (0);
+}
