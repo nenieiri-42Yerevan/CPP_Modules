59bdabccedc979f10a00e04f9392f1e9aa4572bd
Volodya Ismailyan <nenie_iri@mail.ru>
2022-08-11 16:51:37 +0400

    CPP_07_ex01 Add example from check-list to my main

--- CPP_Module_07/ex01/main.cpp
+++ CPP_Module_07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: vismaily <nenie_iri@mail.ru>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/11 12:25:45 by vismaily          #+#    #+#             */
-/*   Updated: 2022/08/11 12:51:07 by vismaily         ###   ########.fr       */
+/*   Updated: 2022/08/11 16:50:08 by vismaily         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,28 @@ void	increment(char &elem)
 	++elem;
 }
 
+class	Awesome
+{
+	public:
+		Awesome(void): _n(42) { return ; }
+		int	get(void) const { return this->_n; }
+	private:
+		int	_n;
+};
+
+std::ostream	&operator<<(std::ostream &o, Awesome const &rhs)
+{
+	o << rhs.get();
+	return o;
+}
+
+template	<typename T>
+void	print(T &x)
+{
+	std::cout << x << std::endl;
+	return ;
+}
+
 int	main()
 {
 	int		int_arr[] = {0, 1, 2, 3, 10, 300, 4};
@@ -44,6 +66,14 @@ int	main()
 	std::cout << "-----------" << std::endl;
 	iter(char_arr, 9, &increment);
 	iter(char_arr, 9, &print_func);
+
+	int		tab[] = {0, 1, 2, 3, 4};
+	Awesome	tab2[5];
+
+	std::cout << "-----------" << std::endl;
+	iter(tab, 5, print);
+	std::cout << "-----------" << std::endl;
+	iter(tab2, 5, print);
 
 	return (0);
 }
